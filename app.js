88dbfda99387981a0338dfcc54395db8b0558a06
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-11 16:32:38 +0100

    functioning snake head

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
 
 let speed = 5
 
+let tileCount = 20
+let tileBlock = canvas.width / tileCount
+let tileSize = tileBlock -2
+let snakeX = 10
+let snakeY = 10
+let yVelocity = 0
+let xVelocity = 0
+
 function drawGame(){
-    console.log(drawGame)
     clearScreen()
+    changePosition()
+    drawSnake()
     setTimeout(drawGame,1000/ speed)
 }
 
@@ -14,9 +23,56 @@ function clearScreen(){
     ctx.fillRect(0,0,canvas.width,canvas.height)
 }
 
+function drawSnake() {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(snakeX*tileBlock,snakeY*tileBlock,tileSize, tileSize)
+}
 
+function changePosition(){
+    snakeX = snakeX + xVelocity
+    snakeY = snakeY + yVelocity
+}
+
+document.body.addEventListener('keydown', keyDown)
+
+function keyDown(event){
+    //up
+    if(event.keyCode == 38) {
+        if(yVelocity == 1)
+            return
+        yVelocity = -1
+        xVelocity = 0
+    }
+    //down
+    if(event.keyCode == 40) {
+        if(yVelocity == -1)
+            return
+        yVelocity = 1
+        xVelocity = 0
+    }
+    //left
+    if(event.keyCode == 37) {
+        if(xVelocity == 1)
+            return
+        yVelocity = 0
+        xVelocity = -1
+    }
+    //right
+    if(event.keyCode == 39) {
+        if(xVelocity == -1)
+            return 
+        yVelocity = 0
+        xVelocity = 1
+    }
+}
+
+drawGame()
 //drawFruit
 //check if the snake eats the fruit - increase size of its tail and relocate the fruit 
 //if (snakeHeadx === fruitX && snakeHead === fruitY)
 //totalTail++
 //fruitPosition()
+
+//build a high score table
+
+
