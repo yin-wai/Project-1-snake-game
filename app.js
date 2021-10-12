ede4ededa4dfe5add14bc0304f80158a6ba9504b
Yin-wai Tse <yin-waitse@Gods-MacBook-Pro.local>
2021-10-12 11:33:19 +0100

    functioning snake game

--- app.js
+++ app.js
@@ -1,21 +1,84 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+class SnakePart {
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+}
 let speed = 5
 
 let tileCount = 20
 let tileBlock = canvas.width / tileCount
 let tileSize = tileBlock -2
+
 let snakeX = 10
 let snakeY = 10
+const snakeParts = []
+let snakeTail = 2 
+
 let yVelocity = 0
 let xVelocity = 0
 
+let foodX = 5
+let foodY = 5
+
+let score = 0
+
 function drawGame(){
+    let result = isGameOver()
+    if(result){
+        return
+    }
     clearScreen()
     changePosition()
+    checkFoodConsumed()
     drawSnake()
+    drawFood()
+    drawScore()
     setTimeout(drawGame,1000/ speed)
+}
+
+function isGameOver(){
+    let gameOver = false
+    
+    if(xVelocity === 0 && yVelocity === 0){
+        return false
+    }
+    if(snakeX < 0){
+        gameOver = true
+    }
+    else if(snakeX === tileBlock){
+        gameOver = true
+    }
+    else if(snakeY < 0) {
+        gameOver = true
+    }
+    else if(snakeY === tileBlock){
+        gameOver = true
+    }
+
+    for(let i=0; i < snakeParts.length; i++){
+        let part = snakeParts[i]
+        if(part.x === snakeX && part.Y === snakeY) {
+            gameeOver = true;
+            break;
+        }
+    }
+
+    if (gameOver) {
+        ctx.fillStyle = 'white'
+        ctx.font = "50px geneva"
+        ctx.fillText("GAME OVER!", canvas.width/4.5, canvas.height/2)
+    }
+    return gameOver
+}
+
+function drawScore(){
+    ctx.fillStyle = "white"
+    ctx.font = "10px verdana"
+    ctx.fillText("Score " + score, canvas.width-50, 10)
 }
 
 function clearScreen(){
@@ -24,6 +87,16 @@ function clearScreen(){
 }
 
 function drawSnake() {
+    ctx.fillStyle = "red"
+    for(let i=0; i < snakeParts.length; i++){
+        let part = snakeParts[i]
+        ctx.fillRect(part.x*tileBlock, part.y*tileBlock, tileSize, tileSize)
+    }
+    snakeParts.push(new SnakePart(snakeX, snakeY))
+    if(snakeParts.length > snakeTail) {
+        snakeParts.shift()
+    }
+
     ctx.fillStyle = 'black'
     ctx.fillRect(snakeX*tileBlock,snakeY*tileBlock,tileSize, tileSize)
 }
@@ -31,6 +104,20 @@ function drawSnake() {
 function changePosition(){
     snakeX = snakeX + xVelocity
     snakeY = snakeY + yVelocity
+}
+
+function drawFood() {
+    ctx.fillStyle = "yellow"
+    ctx.fillRect(foodX* tileBlock, foodY*tileBlock, tileSize, tileSize)
+}
+
+function checkFoodConsumed() {
+    if (foodX === snakeX && foodY === snakeY) {
+        foodX = Math.floor(Math.random() * tileCount)
+        foodY = Math.floor(Math.random() * tileCount)
+        snakeTail++
+        score++
+    }
 }
 
 document.body.addEventListener('keydown', keyDown)
@@ -67,11 +154,6 @@ function keyDown(event){
 }
 
 drawGame()
-//drawFruit
-//check if the snake eats the fruit - increase size of its tail and relocate the fruit 
-//if (snakeHeadx === fruitX && snakeHead === fruitY)
-//totalTail++
-//fruitPosition()
 
 //build a high score table
 
